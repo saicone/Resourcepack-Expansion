69fbce4487f45f025a9243cc4c65af903c414c06
Rubén <44579213+Rubenicos@users.noreply.github.com>
2025-05-05 09:11:59 -0600

    Add persistent cache

--- src/main/java/com/saicone/placeholder/resourcepack/ResourcepackExpansion.java
+++ src/main/java/com/saicone/placeholder/resourcepack/ResourcepackExpansion.java
@@ -14,27 +14,86 @@ import org.bukkit.packs.ResourcePack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class ResourcepackExpansion extends PlaceholderExpansion implements Listener, Cacheable, Cleanable {
 
+    private static final String CACHE_FILE = "resourcepack-expansion-cache.csv.tmp";
+
     private final Map<UUID, PlayerResourcePackStatusEvent.Status> states = new HashMap<>();
+    private final Path cacheFile = Paths.get(System.getProperty("java.io.tmpdir")).resolve(CACHE_FILE);
 
     public ResourcepackExpansion() {
+        // Register listener
         Bukkit.getPluginManager().registerEvents(this, getPlaceholderAPI());
+        // Load cached states
+        try {
+            loadCache();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @EventHandler
-    public void onStatusUpdate(PlayerResourcePackStatusEvent event) {
-        states.put(event.getPlayer().getUniqueId(), event.getStatus());
+    public void loadCache() throws IOException {
+        if (Files.exists(cacheFile)) {
+            try (BufferedReader reader = Files.newBufferedReader(cacheFile)) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    final int index = line.indexOf(',');
+                    if (index < 0) {
+                        continue;
+                    }
+                    final UUID uniqueId = UUID.fromString(line.substring(0, index).trim());
+                    // Ignore offline players
+                    if (Bukkit.getPlayer(uniqueId) == null) {
+                        continue;
+                    }
+                    final PlayerResourcePackStatusEvent.Status status = PlayerResourcePackStatusEvent.Status.valueOf(line.substring(index + 1).trim());
+                    states.put(uniqueId, status);
+                }
+            }
+        }
+    }
+
+    public void saveCache() throws IOException {
+        if (states.isEmpty()) {
+            if (Files.exists(cacheFile)) {
+                Files.delete(cacheFile);
+            }
+            return;
+        } else if (!Files.exists(cacheFile)) {
+            Files.createFile(cacheFile);
+            cacheFile.toFile().deleteOnExit();
+        }
+        try (BufferedWriter writer = Files.newBufferedWriter(cacheFile)) {
+            for (Map.Entry<UUID, PlayerResourcePackStatusEvent.Status> entry : states.entrySet()) {
+                writer.write(entry.getKey().toString());
+                writer.write(",");
+                writer.write(entry.getValue().name());
+                writer.newLine();
+            }
+        }
     }
 
     @Override
     public void clear() {
-        // Using clear just to unregister listener, we should keep it to avoid problems with /papi reload
+        // Unregister listener
         HandlerList.unregisterAll(this);
+        // Save cached states
+        try {
+            saveCache();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        states.clear();
     }
 
     @Override
@@ -55,6 +114,11 @@ public class ResourcepackExpansion extends PlaceholderExpansion implements Liste
     @Override
     public @NotNull String getVersion() {
         return "${version}";
+    }
+
+    @EventHandler
+    public void onStatusUpdate(PlayerResourcePackStatusEvent event) {
+        states.put(event.getPlayer().getUniqueId(), event.getStatus());
     }
 
     @Override
